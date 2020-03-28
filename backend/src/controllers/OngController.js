eb4ce00b12dd9447ba3d691b6f4d4db14ec841c3
Dayana Sabino <sabino.dayana@gmail.com>
2020-03-28 19:49:02 -0300

    Finalização do Back-end com testes

--- backend/src/controllers/OngController.js
+++ backend/src/controllers/OngController.js
@@ -1,5 +1,5 @@
 const connection = require('../database/connection');
-const crypto = require('crypto');
+const generateUniqueId = require('../utils/generateUniqueId');
 /**
  *  Aqui vamos exportar um objeto com os métodos
  */
@@ -14,7 +14,7 @@ module.exports = {
 
     async create(request, response) {
         const { name, email, whatsapp, city, uf } = request.body;
-        const id = crypto.randomBytes(4).toString('HEX');
+        const id = generateUniqueId();
 
         await connection('ongs').insert({
             id,
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -4,21 +4,43 @@ const OngController = require('./controllers/OngController');
 const IncidentController = require('./controllers/IncidentController');
 const ProfileController = require('./controllers/ProfileController');
 const SessionController = require('./controllers/SessionController');
+const { celebrate, Segments, Joi } = require('celebrate');
 
 //Grava novas ongs no banco
-routes.post('/ongs', OngController.create);
+routes.post('/ongs', celebrate({
+    [Segments.BODY]: Joi.object().keys({
+        name: Joi.string().required(),
+        email: Joi.string().required().email(),
+        whatsapp: Joi.string().required().min(10).max(11),
+        city: Joi.string().required(),
+        uf: Joi.string().required().length(2),
+    })
+}), OngController.create);
 //Lista as ongs do banco
 routes.get('/ongs', OngController.index);
 
 //Grava novos casos
+// Validação com celebrate
 routes.post('/incidents', IncidentController.create);
 //Lista novos casos
-routes.get('/incidents', IncidentController.index);
+routes.get('/incidents', celebrate({
+    [Segments.QUERY]: Joi.object().keys({
+        page: Joi.number()
+    })
+}), IncidentController.index);
 //Deleta um caso
-routes.delete('/incidents/:id', IncidentController.delete);
+routes.delete('/incidents/:id', celebrate({
+    [Segments.PARAMS]: Joi.object().keys({
+        id: Joi.number().required(),
+    })
+}), IncidentController.delete);
 
 //Lista os casos por ong
-routes.get('/profiles', ProfileController.index);
+routes.get('/profiles', celebrate({
+    [Segments.HEADERS]: Joi.object({
+        authorization: Joi.string().required(),
+    }).unknown(),
+}), ProfileController.index);
 
 //Rota de Login
 routes.post('/sessions', SessionController.create);
